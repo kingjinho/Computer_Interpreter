7495d7a0436163b7d481cd05c2e288cb41684c3e
kingjinho <jinhoshin319@gmail.com>
2020-08-13 12:58:57 +0900

    lox WIP

--- src/Lox.kt
+++ src/Lox.kt
@@ -1,10 +1,73 @@
+import java.io.BufferedReader
 import java.io.IOException
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+import kotlin.system.exitProcess
 
 class Lox {
-    companion object{
+    companion object {
         @Throws(IOException::class)
-        fun main (args : Array<String>){
-
+        fun main(args: Array<String>) {
+            /**
+             * 2 ways to run code
+             * - start jlox from command line and give it a path to a file, and executes
+             * - start jlox without arguments -> let you enter and execute code one line at a time , a.k.a REPL
+             * */
+            when {
+                args.size > 1 -> {
+                    println("Usage : jlox [script]")
+                    exitProcess(64) //System.exit(64)
+                }
+                args.size == 1 ->
+                    //way 1
+                    runFile(args[0])
+                else ->
+                    //way 2
+                    runPrompt()
+            }
         }
+
+        //path
+        @Throws(IOException::class)
+        private fun runFile(path: String) {
+            val bytes = Files.readAllBytes(Paths.get(path))
+            run(String(bytes, Charset.defaultCharset()))
+        }
+
+        //without arguments
+        @Throws(IOException::class)
+        private fun runPrompt() {
+            val input = InputStreamReader(System.`in`)
+            val reader = BufferedReader(input)
+
+            while (true) {
+                println("> ")
+                val line = reader.readLine()
+                    ?: break //read a line of input from the user on the command line and returns the result
+                run(line)
+            }
+        }
+
+        private fun run(source: String) {
+            val scanner = Scanner(source)
+            val tokens = scanner.scanToken()
+
+            for (token in tokens) {
+                println(token)
+            }
+        }
+
+        fun error(line: Int, message: String) {
+            report(line, "", message);
+        }
+
+        private fun report(line: Int, where: String, message: String) {
+            System.err.println("[line $line] Error $where: $message")
+            hadError = true
+        }
+
     }
 }
